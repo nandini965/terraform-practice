bee38c0426456c2ec01298017b1e584fd53f0338
nandini965 <nandhini965@gmail.com>
2024-12-13 23:23:14 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 module "vpc" {
   source     = "git::https://github.com/nandini965/terraform-practice.git"
   for_each   = var.vpc
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   cidr_block = each.value["cidr_block"]
   vpc_id     = var.vpc_id
   env = var.env
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr = var.default_vpc_cidr
+  tags = var.tags
 }
--- var.tf
+++ var.tf
@@ -3,4 +3,4 @@ variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
-
+variable "tags" {}
