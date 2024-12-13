ed5e0fb634350c661c67c4280ef0cbd52eb19bb9
nandini965 <nandhini965@gmail.com>
2024-12-13 23:59:03 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source           = "git::https://github.com/nandini965/terraform-practice.git"
+  source           = "git::https://github.com/nandini965/tf-module-vpc-practice.git"
   for_each         = var.vpc
   cidr_block       = each.value["cidr_block"]
 
