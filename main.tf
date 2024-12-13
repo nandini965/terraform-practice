0ed84f5e16a65dcb95a78d56b2b465853e63a6ab
nandini965 <nandhini965@gmail.com>
2024-12-13 23:30:18 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "vpc" {
   for_each   = var.vpc
   vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   cidr_block = each.value["cidr_block"]
-  vpc_id     = var.vpc_id
   env = var.env
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr = var.default_vpc_cidr
