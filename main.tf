b5e190ab862bd04877865f408ba7951ef7dffb34
nandini965 <nandhini965@gmail.com>
2024-12-15 16:18:49 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source           = "git::https://github.com/nandini965/tf-module-vpc-practice.git"
   for_each         = var.vpc
   cidr_block       = each.value["cidr_block"]
-   subnets            = each.value["subnets"]
+   #subnets            = each.value["subnets"]
   #default_vpc_id = var.default_vpc_id
   #default_vpc_cidr  = var.default_vpc_cidr
   #env = var.env
