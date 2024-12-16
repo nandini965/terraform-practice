374ec3b9cf884e5be55ce26bf02a51eb716d4ed9
nandini965 <nandhini965@gmail.com>
2024-12-16 09:03:56 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
    subnets            = each.value["subnets"]
   #default_vpc_id = var.default_vpc_id
  # default_vpc_cidr  = var.default_vpc_cidr
-  #env = var.env
- # vpc_id = var.vpc
+  env = var.env
+  vpc_id = var.vpc
 
 }
