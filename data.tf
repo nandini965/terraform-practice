c783042faa58328ad5ed5580e7339c3026c7dd4e
HP <nandhinigoud965@gmail.com>
2025-02-01 21:45:34 +0530

    terraform.tf

--- data.tf
+++ data.tf
@@ -1,10 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
-  owners = ["679593333241"]
-  filter {
-    name   = "name"
-    values = [" CentOS 9 x86_64 LATEST"]  # Amazon Linux 2 AMI
-  }
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      =  ["973714476881"]
 }
-
 data "aws_caller_identity" "identity" {}
