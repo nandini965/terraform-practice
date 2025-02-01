7cac55dec9d4d4a906d71d32e7214bb5546a8c4e
HP <nandhinigoud965@gmail.com>
2025-02-01 17:01:42 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = var.vpc
+  cidr_block = var.cidr
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = "vpc creation"
--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
 
-variable "vpc" {
+variable "cidr" {
   cidr_block = "10.0.0.0/16"
 }
