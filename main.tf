8789691abbefb5697f4e0b3244aa5c90c7d2ea50
HP <nandhinigoud965@gmail.com>
2025-02-01 20:03:33 +0530

    terraform.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "sub1" {
 
 resource "aws_subnet" "sub2" {
   vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.1.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
