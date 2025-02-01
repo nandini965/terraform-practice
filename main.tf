186878443f93a8cfb2e91525f6831a8438f237de
HP <nandhinigoud965@gmail.com>
2025-02-01 16:31:35 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "myvpc" {
 }
 resource "aws_subnet" "sub1" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.0/24"
+  cidr_block = "10.0.0.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -14,7 +14,7 @@ resource "aws_subnet" "sub1" {
 
 resource "aws_subnet" "sub2" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.1.0/24"
+  cidr_block = "10.1.0.0/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
