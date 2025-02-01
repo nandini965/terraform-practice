902936c3951d0969320ce35d97f012352b788ba4
HP <nandhinigoud965@gmail.com>
2025-02-01 21:49:14 +0530

    terraform.tf

--- data.tf
+++ data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      =  ["973714476881"]
+  owners      =  ["679593333241"]
 }
 data "aws_caller_identity" "identity" {}
