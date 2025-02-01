dac9c186fa8a2bc0d648ee2ddf3270fee55a619c
HP <nandhinigoud965@gmail.com>
2025-02-01 19:33:35 +0530

    terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.11.0"
+      version = "~> 5.11.0"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
   region = "us-east-1"
+  # profile = "default" # Uncomment if you have a specific AWS CLI profile
 }
