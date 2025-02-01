0be9130ab3658993d6f2e83e52382fa60b9b40b4
HP <nandhinigoud965@gmail.com>
2025-02-01 19:51:04 +0530

    terraform.tf

--- backend.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "nandini_bucket_for_practice"
-key = "nandini/terraform.tfstate"
-region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "nandini_bucket_for_practice"
-    key = "nandini/terraform.tfstate"
+    key = "roboshop/dev/terraform.tfstate"
     region = "us-east-1"
 
 
