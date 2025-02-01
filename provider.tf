3b4736fb1501bde41aacf947d4405371466bd6be
HP <nandhinigoud965@gmail.com>
2025-02-01 18:20:02 +0530

    filebeat

--- provider.tf
+++ provider.tf
@@ -1,14 +1,13 @@
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.11.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
-provider "aws" {
-  # configuration option
-  region = "us-east-1"
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
