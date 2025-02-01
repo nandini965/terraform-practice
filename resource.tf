45bb944e468dcf53af1e20bbdcfcdb72e5b95ef1
HP <nandhinigoud965@gmail.com>
2025-02-01 21:03:29 +0530

    terraform.tf

--- resource.tf
+++ resource.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "webSg" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.sub2.id
