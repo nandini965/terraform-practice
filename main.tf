fbb2875bb96d996b175d8fc96d84d7a64760d3bc
HP <nandhinigoud965@gmail.com>
2025-02-01 19:32:57 +0530

    terraform.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,46 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = "vpc creation"
+  enable_dns_support   = true
+  tags = {
+    Name = "vpc_creation"
+  }
 }
-resource "aws_subnet" "sub1" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
 
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.1.0.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.1.0.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.myvpc.id
 }
+
 resource "aws_route_table" "RT" {
-vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.myvpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
-resource "route_table_association" "rta1" {
-  subnet_id = aws_subnet.sub1.id
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
   route_table_id = aws_route_table.RT.id
 }
 
-resource "route_table_association" "rta2" {
-  subnet_id = aws_subnet.sub2.id
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
   route_table_id = aws_route_table.RT.id
 }
 
