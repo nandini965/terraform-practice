c7453e73e3ead0fcedf7500bf95034c35aaf2b64
HP <nandhinigoud965@gmail.com>
2025-02-01 14:42:18 +0530

    filebeat

---
+++ backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "nandini_bucket_for_practice"
+key = "nandini/terraform.tfstate"
+region = "us-east-1"
---
+++ resource.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = var.vpc
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Websg"
+  }
+}
+
+  resource "aws_instance" "myinstance" {
+    ami                     = "08b3552b911ba4752"
+    instance_type  = "t2.small"
+    security_groups     = [aws_security_group.webSg]
+    subnet_id     = var.subnet_id
+  }
